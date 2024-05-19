dbbca26b9c55c809cafc6dcddd48b40fb51d4a71
Soumyadip Maity <115390789+Soumyadipmaity1@users.noreply.github.com>
2024-05-19 07:03:54 +0000

    Add JavaScript data types

---
+++ JavaScript_Basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use script"; // Treat all JS code as newer ES6 version
+
+console.log(3+4);   // code readabililty should be high and code should be self-explanatory
+
+  let name = 'Soumyadip';
+    let age = 30;
+    let isApproved = true;
+
+    console.log(typeof name);
+    console.log(typeof 30);
+    console.log(typeof null); // object 
+    console.log(typeof undefined); // undefined
