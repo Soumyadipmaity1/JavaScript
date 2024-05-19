86f74c2fd3fc42b739621eb65bcffffd746a4ee6
Soumyadip Maity <115390789+Soumyadipmaity1@users.noreply.github.com>
2024-05-19 07:34:59 +0000

    Add JavaScript data type operations and examples

---
+++ JavaScript_Basics/04_DataTypesOperation.js
@@ -0,0 +1,27 @@
+let value = 10;
+// console.log(value);
+let negativeValue = -value;
+// console.log(negativeValue);
+
+// console.log(3 + 3);
+// console.log(3 - 3);
+// console.log(3 * 3);
+// console.log(3 / 3);
+// console.log(3 % 3);
+// console.log(3 ** 3);
+// console.log(3 + 3 * 3);
+
+let x1 = "Soumyadip";
+let x2 = "Maity";
+
+let result = x1 + x2;
+// console.log(result);
+
+console.log("1" + 1);
+console.log(1 + "1");
+console.log("2" + 1 + 1);
+console.log(1 + 1 + "2");
+console.log(3 + 4 * 5 % 3);
+console.log(true);
+console.log(+false);
+console.log(+"");
