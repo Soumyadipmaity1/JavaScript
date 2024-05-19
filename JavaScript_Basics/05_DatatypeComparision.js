041cfc9d8a3b09070b6ffd28e97cf52e19fa27b4
Soumyadip Maity <115390789+Soumyadipmaity1@users.noreply.github.com>
2024-05-19 07:46:18 +0000

    Add code for comparing JavaScript data types

---
+++ JavaScript_Basics/05_DatatypeComparision.js
@@ -0,0 +1,20 @@
+
+
+
+
+console.log("5"> 5); // false
+console.log("02" > 1); // true
+console.log("2" > 1); // true
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+console.log(undefined > 0); // false
+console.log(undefined == 0); // false
+console.log(undefined >= 0); // false
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+
+console.log("2" > true); // true
