9199d462fae3e446c70ceffbfd59f39bb77f6d5e
Soumyadip Maity <115390789+Soumyadipmaity1@users.noreply.github.com>
2024-05-22 06:19:06 +0000

    Add JavaScript object and JSON examples

---
+++ JavaScript_Intermediate/05_ObjectAndJSON.js
@@ -0,0 +1,27 @@
+const course ={
+    name: 'JavaScript',
+    duration: '3 months',
+    topics: ['ES6', 'React', 'Node'],
+}
+
+const{name} = course;
+console.log(name);
+
+const{topics: skill} = course;
+console.log(skill); 
+
+
+//JSON Format
+
+// {
+//     "name": "JavaScript",
+//     "duration": "3 months",
+//     "topics": ["ES6", "React", "Node"]
+// }
+
+[
+    {},
+    {},
+    {},
+    {}
+]
