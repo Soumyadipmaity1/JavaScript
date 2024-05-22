b1379e0440389950310420ff7e75eb358bd0f8e5
Soumyadip Maity <115390789+Soumyadipmaity1@users.noreply.github.com>
2024-05-22 06:49:11 +0000

    Add JavaScript function examples and methods

---
+++ 03_JavaScript_Basics/01_function.js
@@ -0,0 +1,54 @@
+
+
+// function AddtwoNumbers(a, b)  //parameters
+// {
+//     console.log(a + b);
+// } 
+
+AddtwoNumbers(10, 20); //arguments
+
+const result = AddtwoNumbers(10, 20);
+console.log(result); //undefined
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
