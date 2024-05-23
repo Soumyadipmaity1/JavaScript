d5592835c5778eca3a8328f2feb6d03f03f879b8
Soumyadip Maity <115390789+Soumyadipmaity1@users.noreply.github.com>
2024-05-23 06:36:22 +0000

    Add JavaScript scope level example

---
+++ 03_JavaScript_Basics/04_scopeLevel.js
@@ -0,0 +1,38 @@
+function one (){
+    const username = 'Soumyadip';
+
+    function two(){
+        const website = 'soumyadipmaity1.github.io';
+        console.log(username);
+    }
+    // console.log(website);
+    // two();
+}
+// one();
+
+if(true){
+    const username="Soumyadip";
+    if (username==="Soumyadip"){
+        const website = "soumyadipmaity1.github.io";
+        // console.log(username + " " + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// +++++++++++++++++++++++++++++ Interesting ++++++++++++++++++++++++++++++++++++++++++++++
+
+function addone(num){
+return num+1;
+}
+addone(5);
+
+const addTwo = function(num){
+    return num+2;
+}
+
+addTwo(5);
+
+console.log(addTwo(5));
+console.log(addone(5));
