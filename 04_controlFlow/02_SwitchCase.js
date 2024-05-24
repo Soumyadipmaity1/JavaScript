3fbdf4a1389367209ed18f4588af633ca89d9c4b
Soumyadip Maity <115390789+Soumyadipmaity1@users.noreply.github.com>
2024-05-24 06:16:34 +0000

    feat: Add switch case example for month selection

---
+++ 04_controlFlow/02_SwitchCase.js
@@ -0,0 +1,86 @@
+ 
+ const month = 2;
+
+    switch (month) {
+        case 1:
+            console.log("January");
+            break;
+        case 2:
+            console.log("February");
+            break;
+        case 3:
+            console.log("March");
+            break;
+        case 4:
+            console.log("April");
+            break;
+        case 5:
+            console.log("May");
+            break;
+        case 6:
+            console.log("June");
+            break;
+        case 7:
+            console.log("July");
+            break;
+        case 8:
+            console.log("August");
+            break;
+        case 9:
+            console.log("September");
+            break;
+        case 10:
+            console.log("October");
+            break;
+        case 11:
+            console.log("November");
+            break;
+        case 12:
+            console.log("December");
+            break;
+        default:
+            console.log("Invalid month");
+        }
+
+
+        const month2 ="january";
+        switch (month2) {
+            case "january":
+                console.log("January");
+                break;
+            case "february":
+                console.log("February");
+                break;
+            case "march":
+                console.log("March");
+                break;
+            case "april":
+                console.log("April");
+                break;
+            case "may":
+                console.log("May");
+                break;
+            case "june":
+                console.log("June");
+                break;
+            case "july":
+                console.log("July");
+                break;
+            case "august":
+                console.log("August");
+                break;
+            case "september":
+                console.log("September");
+                break;
+            case "october":
+                console.log("October");
+                break;
+            case "november":
+                console.log("November");
+                break;
+            case "december":
+                console.log("December");
+                break;
+            default:
+                console.log("Invalid month");
+            }
