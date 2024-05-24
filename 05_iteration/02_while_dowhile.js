a917897e71871f28184d0510228db58e1881e9f0
Soumyadip Maity <115390789+Soumyadipmaity1@users.noreply.github.com>
2024-05-24 06:57:13 +0000

    Add while and do-while loop examples

---
+++ 05_iteration/02_while_dowhile.js
@@ -0,0 +1,21 @@
+// syntax
+// while (condition) {
+    
+// }
+
+// do {
+    
+// } while (condition);
+
+let index=0;
+while(index<5) {
+    console.log(index);
+   index= index+2;
+}
+
+let score = 0;
+
+do {
+    console.log("score is", score);
+    score++;
+} while (score<10);
